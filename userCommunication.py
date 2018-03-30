7a5e502919c7545f838671bb085d8609d8ccfff2
EmilioAVazquez <emibars97@gmail.com>
2018-03-30 17:32:04 -0500

    User Communication File added

---
+++ userCommunication.py
@@ -0,0 +1,7 @@
+def generateComunication(IPAdress)
+//Inpute : Inputs the IP adress of the other computer
+//Output : Ouputs a communication object
+def sendMessage(message, communicationObject)
+//Inpute : The message to send and the communicationObject
+//Output : Void
+
